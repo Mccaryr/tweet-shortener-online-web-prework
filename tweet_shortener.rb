995b26c01ee16a8967728138837492bfe6a2da7f
Robert McCary <robert.mccary@yahoo.com>
2018-12-07 01:27:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+
+
+
+def word_substituter(tweet)
+  answer = ""
+  dictionary = {"Hey"=> "hi", "to"=> "2", "two"=> "2", "too"=> "2", "for"=> "4", "four"=> "4", "be"=> "b", "you"=> "u", "at"=> "@", "and"=> "&"}
+tweet_array = tweet.split(" ")
+# tweet_array << tweet.to_a
+  tweet_array.map { |word|
+    dictionary.each do |k,v|
+      # puts "k = #{k}"
+      # puts "word = #{word}"
+      if word == k
+        puts "k = #{k}"
+        puts "word = #{word}"
+        v
+        break
+      end
+    end
+}
+  tweet_array.each{ |word| answer << " #{word}"}
+  answer[1..-1]
+  end
